969d79f1911b6bba1164585acdcd6971c015e402
al4n1 <164919407+al4n1@users.noreply.github.com>
2024-11-25 13:29:10 -0300

    aaaa

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-function   crieCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container')
-    let cartao = document.createElement('article')
-    cartao.className = ('cartao')
+function  criaCartao(categoria, pergunta, resposta) {
+  let container = document.getElementById('container')
+  let cartao = document.createElement('article')
+  cartao.className = ('cartao')
 
-    cartao.innerHTML =
-    <div class="cartao__conteudo">
-    <h3>$(categoria)</h3>
-      <div class="cartao__conteudo__pergunta">
-       <p>%(resposta)</p>
-      </div>
-     <div class="cartao__conteudo__pergunta">
-     <p>$(resposta)</p>
-     </div>
-     </div> 
+  cartao.innerHTML = 
+      <div class="cartao__conteudo">
+        <h3>$(categoria)</h3>
+        <div class="cartao__conteudo__pergunta">
+        <p> %(pergunta)</p>
+        </div>
+       <div class="cartao__conteudo__pergunta">
+       <p> $(resposta)</p>
+        </div>
+       </div>
 
-   let respostaEstavisivel = false
+  let respostaEstavisível = false 
 
-   function viracartao(){
-    respostaEstavisivel = IrespostaEstavisivel 
-    cartao.classList.toggle('active'. respostaEstavisivel)
-   }
+  function viracartao(){
+      respostaEstavisível = IrespostaEstavisível
+      cartao.classList.toggle('active', respostaEstavisível)
+  }
+  cartao.addEventListener('click', viraCartao)
 
+       container.appendChild(cartao)
 
-
-container.appendChild(cartao)
-
-}
-
+  }
 
